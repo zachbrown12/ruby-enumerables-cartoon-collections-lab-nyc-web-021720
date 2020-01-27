e783d968987b7193a747227842cac018ce136ba2
Zach Brown <zachmb22@gmail.com>
2020-01-27 01:26:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 
 def summon_captain_planet(array)
-  array.map {|word| "#{calls.capitalize}!"
+  array.map {|word| "#{calls.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
