186a2d5d5fdfe164d0f094e32d49b4970f7ed78c
Zach Brown <zachmb22@gmail.com>
2020-01-27 01:37:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ return false
   
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
