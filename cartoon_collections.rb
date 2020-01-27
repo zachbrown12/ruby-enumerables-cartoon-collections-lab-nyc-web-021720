5d0a7a78a3795cb459be978ef6e3e1f85360c4c9
Zach Brown <zachmb22@gmail.com>
2020-01-27 01:33:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,14 @@ def summon_captain_planet(array)
   array.map {|word| "#{word.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |word|
+   if word.length > 4
+     true
+   end
+  end
+false
+  
 end
 
 def find_the_cheese# code an argument here
