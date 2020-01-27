3d92210022b05651bbb5879178faac5cccec4b4b
Zach Brown <zachmb22@gmail.com>
2020-01-27 01:21:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   index = 0
-  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
 
