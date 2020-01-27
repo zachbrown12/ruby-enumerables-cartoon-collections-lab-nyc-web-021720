3aed9be5dce00b02f504aa9392bda5422c0a0962
Zach Brown <zachmb22@gmail.com>
2020-01-27 01:25:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(array)
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|word| 
 end
 
 def long_planeteer_calls# code an argument here
